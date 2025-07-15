ed3c72560e101235b9c98b58715bf99e5847e17b
dilis117 <lauri_lat@hotmail.com>
2025-07-15 12:54:06 +0300

    Create scan_input_and_print.cpp

---
+++ scan_input_and_print.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	//declare int-variables
+	int num1, num2, sum;
+	//print first prompt to console
+	cout << "Give first number:";
+	//scan for user input and save integer to 'num1'-variable
+	cin >> num1;
+	//print second prompt to console
+	cout << "Give second number:";
+	//scan for user input and save integer to 'num2'-variable
+	cin >> num2;
+	//sum 'num1' and 'num2', then save the result into 'sum'-variable
+	summa = num1 + num2;
+	//print message and result to consol
+	cout << "Antamiesi lukujen summa on " << sum;
+}
